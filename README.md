90626d1b684c31f686261e81da8da38b1fcc2a66
negative-zer0 <iunixpl69@gmail.com>
2021-04-29 08:41:26 +0200

    Added more features

---
+++ README.md
@@ -0,0 +1,5 @@
+# Height calculator tool
+
+## Features
+- Checking height
+- Checking weight
--- src/main/java/net/unix/heightcalculator/GUI.java
+++ src/main/java/net/unix/heightcalculator/GUI.java
@@ -13,20 +13,32 @@ import java.util.Arrays;
 public class GUI extends JPanel {
 
     public GUI() {
-        var button = new JButton("Apply");
+        var button1 = new JButton("Apply height");
         var label1 = new JLabel("Input your height");
+        var textField1 = new JTextField(5);
         var label2 = new JLabel("cm");
-        var textField = new JTextField(5);
+
+        var button2 = new JButton("Apply weight");
+        var label3 = new JLabel("Input your weight");
+        var textField2 = new JTextField(5);
+        var label4 = new JLabel("kg");
 
         setPreferredSize(new Dimension(310, 140));
         setLayout(null);
 
-        button.addActionListener(btn -> PopupUtil.showPopup(String.format("Your height is %scm!", textField.getText())));
-        addAll(button, label1, label2, textField);
-        button.setBounds(205, 90, 90, 30);
-        label1.setBounds(15, 40, 100, 25);
-        label2.setBounds(255, 40, 25, 20);
-        textField.setBounds(115, 40, 135, 30);
+        button1.addActionListener(btn -> PopupUtil.showPopup(String.format("Your height is %scm!", textField1.getText())));
+        button2.addActionListener(btn -> PopupUtil.showPopup(String.format("Your weight is %skg!", textField2.getText())));
+        addAll(button1, label1, textField1, label2, button2, label3, textField2, label4);
+        button1.setBounds(170, 90, 130, 30);
+        label1.setBounds(15, 15, 100, 25);
+        textField1.setBounds(115, 15, 135, 30);
+        label2.setBounds(255, 15, 25, 20);
+
+        button2.setBounds(10, 90, 130, 30);
+        label3.setBounds(15, 55, 100, 25);
+        textField2.setBounds(115, 55, 135, 30);
+        label4.setBounds(255, 55, 25, 20);
+
     }
 
     private void addAll(Component... components) {
